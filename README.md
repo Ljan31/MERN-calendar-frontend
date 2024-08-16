b4452aa2d93d62ad252a82e29432489183907654
Ljan31 <limberg.mamani@sarys.ar>
2024-08-16 10:10:33 -0400

    first commit frontend

---
+++ README.md
@@ -0,0 +1,13 @@
+# Calendar APP
+
+## Development pasos
+1. Renombrar el archivo `.env.template` por `.env`
+2. Hacer los cambios respectivos en las variables de entorno.
+
+```
+VITE_API_URL=http://localhost:4000/api
+```
+
+https://cdnjs.com/libraries/font-awesome
+https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css
+https://www.npmjs.com/package/react-big-calendar
---
+++ src/hooks/useCalendarStore.js
@@ -0,0 +1,45 @@
+import { useDispatch, useSelector } from "react-redux";
+import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store";
+
+
+export const useCalendarStore = () => {
+  
+  const dispatch = useDispatch();
+  const { events, activeEvent } = useSelector( state => state.calendar );
+  
+  const setActiveEvent = ( calendarEvent ) => {
+    dispatch( onSetActiveEvent( calendarEvent ) );
+  }
+
+  const startSavingEvent = async( calendarEvent ) => {
+    // todo: llegar al backend
+
+    // todo bien
+    if( calendarEvent._id ){
+      // update
+      dispatch( onUpdateEvent( {...calendarEvent} ) );
+    }else{
+      // crear
+      dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }) );
+    }
+
+  }
+
+  const startDeletingEvent = () => {
+    // todo: llegar al backend
+    dispatch( onDeleteEvent() );
+  }
+
+
+  return {
+    //* propiedades
+    events,
+    activeEvent,
+    hasEventSelected: !!activeEvent,
+
+    //* metodos
+    setActiveEvent,
+    startSavingEvent,
+    startDeletingEvent,
+  }
+}
